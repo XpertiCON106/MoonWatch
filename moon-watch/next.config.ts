85c920ae5b3d480431c12e5e88f3d58cd9493b19
XpertiCON106 <tahalil106@gmail.com>
2025-04-26 17:36:05 -0400

    getting ready for aws deployment

--- moon-watch/next.config.ts
+++ moon-watch/next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  poweredByHeader: false,
+  reactStrictMode: true,
+  images: {
+    domains: [],
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
